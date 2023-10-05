2e194f7d3fecd852dd4c53fecd5b331b890f2560
LC Andy <92437260+loopcreativeandy@users.noreply.github.com>
2023-10-05 16:38:50 +0200

    adding file for interacting with new web3 js
    note that this library might change and that code might stop working

---
+++ web3v2/experimental.ts
@@ -0,0 +1,68 @@
+import {AccountRole, Base58EncodedAddress, IAccountMeta, IInstruction, appendTransactionInstruction,createDefaultRpcTransport, createSolanaRpc,createTransaction } from "@solana/web3.js"
+import {generateKeyPair, getAddressFromPublicKey , getBase64EncodedWireTransaction, signTransaction, setTransactionFeePayer, setTransactionLifetimeUsingBlockhash} from "@solana/web3.js"
+import '@solana/webcrypto-ed25519-polyfill';
+
+import {LamportsUnsafeBeyond2Pow53Minus1, assertIsLamports} from "@solana/rpc-core"
+
+
+const crypto = require('crypto').webcrypto;
+
+(async()=>{
+    const myAddress = 'DTnUKgY1dq447iaehoCVpoBEe6S98qspRTBbGm9ZxKZb' as Base58EncodedAddress;
+    
+    const rpc = createSolanaRpc({transport: createDefaultRpcTransport({ url: 'https://devnet.helius-rpc.com/?api-key=06c25b64-3d11-40ca-82d8-347a062e74f2' })});
+
+    const kp = await generateKeyPair();
+    const publicKey = await getAddressFromPublicKey(kp.publicKey);
+    console.log(publicKey)
+
+    // const result = await rpc.getBalance(myAddress).send();
+    // const balanceInLamports = result.value;
+    // console.log('Balance of System Program account in Lamports: ', balanceInLamports);
+
+    const reqLamports = BigInt("100000000") as LamportsUnsafeBeyond2Pow53Minus1
+    assertIsLamports(reqLamports);
+    await rpc.requestAirdrop(publicKey, reqLamports).send();
+
+    const tx = createTransaction({version: 0});
+
+    const data = Buffer.alloc(12);
+    data.writeUint32LE(2);
+    data.writeBigInt64LE(BigInt("10000000"),4)
+
+    const ix: IInstruction = {
+        programAddress: '11111111111111111111111111111111' as Base58EncodedAddress,
+        accounts: [
+            {
+                address: publicKey, //await getAddressFromPublicKey(kp.publicKey),
+                role: AccountRole.WRITABLE_SIGNER
+            },
+            {
+                address: myAddress,
+                role: AccountRole.WRITABLE
+            },
+        ],
+        data: data
+    }
+    
+    const txWithIx = appendTransactionInstruction(ix, tx);
+
+    
+    const bh = await rpc.getLatestBlockhash().send();
+    console.log(bh);
+    
+    const txWithFP = setTransactionFeePayer(publicKey,txWithIx)
+    const unsignedTX = setTransactionLifetimeUsingBlockhash(bh.value, txWithFP);
+    const signedTX = await signTransaction(kp, unsignedTX);
+    const encodedTX = getBase64EncodedWireTransaction(signedTX)
+
+    console.log("waiting...")
+    await new Promise(_ => setTimeout(_, 30000));
+    console.log("lfg")
+
+    const sx = await rpc.sendTransaction(encodedTX, {encoding: 'base64'}).send();
+    console.log(sx);
+
+
+
+})();
