48e1ab11e1d5eb078b03c3cf3583d4ec8ac3af98
LC Andy <92437260+loopcreativeandy@users.noreply.github.com>
2023-12-21 18:10:51 +0100

    Create mpl_token22.ts

---
+++ metadataUpdate/mpl_token22.ts
@@ -0,0 +1,129 @@
+
+import { createV1, updateV1 ,Collection, CreateMetadataAccountV3InstructionAccounts, CreateMetadataAccountV3InstructionDataArgs, Creator, MPL_TOKEN_METADATA_PROGRAM_ID, UpdateMetadataAccountV2InstructionAccounts, UpdateMetadataAccountV2InstructionData, Uses, createMetadataAccountV3, updateMetadataAccountV2, findMetadataPda, CreateV1InstructionAccounts, CreateV1InstructionData, TokenStandard, CollectionDetails, PrintSupply, UpdateV1InstructionData, UpdateV1InstructionAccounts, Data} from "@metaplex-foundation/mpl-token-metadata";
+import * as web3 from "@solana/web3.js";
+import { PublicKey, createSignerFromKeypair, none, percentAmount, publicKey, signerIdentity, some } from "@metaplex-foundation/umi";
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { fromWeb3JsKeypair, fromWeb3JsPublicKey} from '@metaplex-foundation/umi-web3js-adapters';
+import * as bs58 from "bs58";
+
+const SPL_TOKEN_2022_PROGRAM_ID: PublicKey = publicKey(
+    'TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb'
+  );
+
+  
+export function loadWalletKey(keypairFile:string): web3.Keypair {
+    const fs = require("fs");
+    const loaded = web3.Keypair.fromSecretKey(
+      new Uint8Array(JSON.parse(fs.readFileSync(keypairFile).toString())),
+    );
+    return loaded;
+  }
+
+const INITIALIZE = true;
+
+async function main(){
+    console.log("let's name some token-22 tokens in 2024!");
+    const myKeypair = loadWalletKey("AndyUCWqhEnEMqHAByoRSHz2mvQxdyXyki9UQ7YCrTBY.json");
+    const mint = new web3.PublicKey("SD2idHukkWPziSuhVnD6yf2gNbgzGYzyvv7787ZvazK")//"SDTHBG48VGNoGS1U2ArnvMUZ3dxyGr1F4TT1ojD4QDB");
+
+    const umi = createUmi("https://api.devnet.solana.com");
+    const signer = createSignerFromKeypair(umi, fromWeb3JsKeypair(myKeypair))
+    umi.use(signerIdentity(signer, true))
+
+    const ourMetadata = { // TODO change those values!
+        name: "Silly Dragon Token Extended", 
+        symbol: "SDT",
+        uri: "https://raw.githubusercontent.com/loopcreativeandy/video-tutorial-resources/main/metadataUpdate/metadata.json",
+    }
+    if(INITIALIZE){
+        const onChainData = {
+            ...ourMetadata,
+            // we don't need that
+            sellerFeeBasisPoints: percentAmount(0,2),
+            creators: none<Creator[]>(),
+            collection: none<Collection>(),
+            uses: none<Uses>(),
+        }
+        const accounts: CreateV1InstructionAccounts = {
+            mint: fromWeb3JsPublicKey(mint),
+            splTokenProgram: SPL_TOKEN_2022_PROGRAM_ID
+        }
+        const data: CreateV1InstructionData = {
+            ...onChainData,
+            isMutable: true,
+            discriminator: 0,
+            tokenStandard: TokenStandard.Fungible,
+            collectionDetails: none<CollectionDetails>(),
+            ruleSet: none<PublicKey>(),
+            createV1Discriminator: 0,
+            primarySaleHappened: true,
+            decimals: none<number>(),
+            printSupply: none<PrintSupply>(),
+        }
+        const txid = await createV1(umi, {...accounts, ...data}).sendAndConfirm(umi);
+        console.log(bs58.encode(txid.signature))
+    } else {
+        const onChainData = {
+            ...ourMetadata,
+            sellerFeeBasisPoints: 0,
+            creators: none<Creator[]>(),
+            collection: none<Collection>(),
+            uses: none<Uses>(),
+        }
+        const accounts: UpdateV1InstructionAccounts = {
+            mint: fromWeb3JsPublicKey(mint),
+        }
+        const data = {
+            discriminator: 0,
+            data: some<Data>(onChainData),
+            updateV1Discriminator: 0,
+        }
+        const txid = await updateV1(umi, {...accounts, ...data}).sendAndConfirm(umi);
+        console.log(bs58.encode(txid.signature))
+    }
+
+
+
+
+    // const seed1 = Buffer.from("metadata", "utf-8");
+    // const seed2 = Buffer.from(MPL_TOKEN_METADATA_PROGRAM_ID.toBytes());
+    // const seed3 = Buffer.from(mint.toBytes());
+    // const [metadataPDA, _bump] = web3.PublicKey.findProgramAddressSync([seed1, seed2, seed3], MPL_TOKEN_METADATA_PROGRAM_ID);
+    // const accounts = {
+    //     metadata: metadataPDA,
+    //     mint,
+    //     mintAuthority: myKeypair.publicKey,
+    //     payer: myKeypair.publicKey,
+    //     updateAuthority: myKeypair.publicKey,
+    // }
+    // let ix;
+    // if (INITIALIZE) {
+
+    //     const args : mpl.CreateMetadataAccountV3InstructionArgs =  {
+    //         createMetadataAccountArgsV3: {
+    //             data: dataV2,
+    //             isMutable: true,
+    //             collectionDetails: null
+    //         }
+    //     };
+    //     ix = mpl.createCreateMetadataAccountV3Instruction(accounts, args);
+    // } else {
+    //     const args =  {
+    //         updateMetadataAccountArgsV2: {
+    //             data: dataV2,
+    //             isMutable: true,
+    //             updateAuthority: myKeypair.publicKey,
+    //             primarySaleHappened: true
+    //         }
+    //     };
+    //     ix = mpl.createUpdateMetadataAccountV2Instruction(accounts, args)
+    // }
+    // const tx = new web3.Transaction();
+    // tx.add(ix);
+    // const connection = new web3.Connection("https://api.mainnet-beta.solana.com");
+    // const txid = await web3.sendAndConfirmTransaction(connection, tx, [myKeypair]);
+    // console.log(txid);
+
+}
+
+main();
